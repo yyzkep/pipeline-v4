fa087551d085e9e0c5fda24cc744abfb9b31da6a
yyzkep <yyzkep@gmail.com>
2025-09-08 21:28:36 +0000

    lmao

--- app/features/esp/esp.cpp
+++ app/features/esp/esp.cpp
@@ -28,7 +28,7 @@ void c_esp::draw_players()
 		if (i == ctx.entities.local_player->ent_idx() && !config::esp_player[3].active)
 			continue;
 
-		client_entity &entity = ctx.interfaces.entity_list->get_client_entity(i);
+		client_entity* entity = ctx.interfaces.entity_list->get_client_entity(i);
 
 		if (!entity)
 			continue;
@@ -39,16 +39,16 @@ void c_esp::draw_players()
 		if (entity->is_dormant())
 			continue;
 
-		base_player &player = entity->as<base_player>();
+		base_player* player = entity->as<base_player>();
 
 		if (!ctx.tf2.get_entity_bounds(player, x, y, w, h))
 			continue;
 
 		player_info_t pi;
-		if (ctx.interfaces.engine->get_player_info(player->ent_idx(), &pi) && config::esp_player->name)
+		if (ctx.interfaces.engine->get_player_info(player->ent_idx(), &pi) && config::esp_player.name)
 			ctx.renderer.render_queue.string(FONTS::FONT_ESP, x + (w / 2), y, color_t::white(), horizontal, pi.name);
 
-		if (config::esp_player->box)
+		if (config::esp_player.box)
 			ctx.renderer.render_queue.rect(x, y, w, h, config::esp_player->box_color);
 	}
 }
