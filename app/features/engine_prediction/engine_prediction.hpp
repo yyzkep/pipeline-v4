1f7c1dfa4bcf7c81d68108f77e794253e1ccf835
yyzkep <yyzkep@gmail.com>
2025-09-09 04:19:35 -0300

    engine pred, embrace the darkness theme and notifications

---
+++ app/features/engine_prediction/engine_prediction.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class c_engine_prediction {
+	//todo:
+	//this
+	//gambled the method, ended up being ctx.interfaces.pred->run_command()... simplest of them all, LETS FUCKING GOOOOOOOOOOOOO
+};
